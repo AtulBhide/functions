3c7c9a27436cbe4aeba3a48a4487d5ac4b7dff3b
Atul Bhide <35613257+AtulBhide@users.noreply.github.com>
2023-05-05 08:55:21 -0700

    1. Specifying return value type 2. Using last line without ";" for implicit return value

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+
+    another_function(9);
+
+    let x = five();
+    println!("Function five(): The value of x is {x}");
+}
+
+fn another_function(x: i32) {
+    println!("Another function: The value of x is {x}");
+}
+
+fn five() -> i32 {
+    5
 }
